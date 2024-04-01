da8049f3543f82b2af03ec88be31dd5dc954c3a8
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-01 21:46:47 +0500

    jhjk

--- index.js
+++ index.js
@@ -4,54 +4,56 @@ import inquirer from "inquirer";
 let balance = 10000; //Dollars
 let myPin = 160397;
 let pinAnswer = await inquirer.prompt([{
-        name: "pin", message: "Enter your Pin", type: "number",
+        name: "pin", message: "Enter your Pin Code ", type: "number"
     }]);
 if (pinAnswer.pin === myPin) {
-    console.log("Correct Pin Code!!!");
-}
-else {
-    console.log("Your Pin Code is Incorrect");
-}
-console.log("Your current balance is Now");
-let Operation = await inquirer.prompt([{
-        name: "operations", message: "Please select one operation", "type": "lists",
-        choices: ["Withraw", "Check Balance", "Fastcash"],
-    }]);
-//step no 2//
-if (Operation.operations === "Withraw") {
-    let amountAns = await inquirer.prompt([{
-            name: "amount", message: "Enter your amount you want to withraw", type: "number"
-        }]);
-    if (amountAns.amount < balance) {
-        (balance -= amountAns.amount);
-        console.log("Now your  balance is: " + balance);
+    console.log("Correct Pin Code!!! You Log in Successfully!");
+    let OperationAns = await inquirer.prompt([
+        {
+            name: "operation", message: "Please select one operation", type: "list",
+            choices: ["Withraw Amount", "Check Balance"],
+        }
+    ]);
+    //step no 2//
+    if (OperationAns.operation === "Withraw Amount") {
+        let withrawAns = await inquirer.prompt([
+            {
+                name: "withrawMethod", message: "Select a Withraw Method", type: "number", choices: ["Fast Cash", "Enter Amount"]
+            }
+        ]);
+        if (withrawAns.withrawMethod === "Fast Cash") {
+            let fastCashAns = await inquirer.prompt([
+                {
+                    name: "fastCash", type: "list", message: "Select Amount", choices: [1000, 2000, 5000, 10000]
+                }
+            ]);
+            if (fastCashAns.fastCash > balance) {
+                console.log("Insufficent Balance");
+            }
+            else {
+                balance -= fastCashAns.fastCash;
+                console.log(`${fastCashAns.fastCash} withraw successfully`);
+                console.log("Now your remaining balance is: " + balance);
+            }
+        }
+        else if (withrawAns.withrawMethod === "Enter Amount") {
+            let amountAns = await inquirer.prompt([
+                {
+                    name: "amount", type: "number", message: "Enter amount You want"
+                }
+            ]);
+            if (amountAns.amount > balance) {
+                console.log("Insufficient Balance");
+            }
+            else {
+                balance -= amountAns.amount;
+                console.log(`${amountAns.amount} withraw successfully`);
+                console.log("Now your remaining balance is:  " + balance);
+            }
+        }
     }
-    else
-        console.log("Insufficient Balance");
+    //step n0 3//
+    else {
+        console.log("Pin Code is Incorrect Try Again");
+    }
 }
-//step n0 3//
-if (Operation.operations === "Check Balance") {
-    console.log("Your current balance is: " + balance);
-}
-//step no 4/
-//         (Operation.operations === "Fastcash") ;
-let fast = await inquirer.prompt([{
-        name: "fast",
-        message: "How much money you want to withraw", "type": "lists",
-        choices: ["1000", "2000", "5000", "8000", "10000"],
-    }]);
-if (fast.fast_Operations === "1000")
-    balance -= fast.fast_Operation;
-console.log(`Your remaining balance is${balance}`);
-if (fast.fast_Operations === "2000")
-    balance -= fast.fast_Operation;
-console.log(`Your remaining balance is${balance}`);
-if (fast.fast_Operations === "5000")
-    balance -= fast.fast_Operation;
-console.log(`Your remaining balance is${balance}`);
-if (fast.fast_Operations === "8000")
-    balance -= fast.fast_Operation;
-console.log(`Your remaining balance is${balance}`);
-if (fast.fast_Operations === "10000")
-    balance -= fast.fast_Operation;
-console.log(`Your remaining balance is${balance}`);
