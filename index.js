44993d643f11ec33fa3ce36d99785e2871245af5
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-02 21:30:23 +0500

    updated

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 import inquirer from "inquirer";
 let myBalance = 10000;
-let pinCode = 160397;
+const pinCode = 4545;
 async function ATM() {
-    {
-        console.log("Welcome to Code with Mehwishnaz");
-    }
     let myAns = await inquirer.prompt([
         {
             name: "userPin",
@@ -13,63 +10,68 @@ async function ATM() {
             message: "Enter your pin",
         },
     ]);
-    if (pinCode === myAns.userPin)
+    if (parseInt(myAns.userPin) === pinCode) {
         console.log("Your pin is correct!");
-    let options = await inquirer.prompt([
-        {
-            name: "menu",
-            type: "list",
-            message: "Please select an option",
-            choices: ["Withdraw", "FastCash", "Balance Inquiry", "Other Transaction"],
-        },
-    ]);
-    if (options.menu === "Withdraw") {
-        let amount = await inquirer.prompt([
+        let options = await inquirer.prompt([
             {
-                name: "amount",
-                type: "number",
-                message: "Please enter amount:",
-            },
-        ]);
-        if (amount.amount < myBalance) {
-            (myBalance -= amount.amount);
-            console.log(`Your Available Balance is ${myBalance}`);
-        }
-        else {
-            console.log(`Insufficient Balance`);
-        }
-    }
-    else if (options.menu === "FastCash") {
-        let fastCash = await inquirer.prompt([
-            {
-                name: "fastCash",
+                name: "menu",
                 type: "list",
-                message: "Select Cash",
-                choices: ["500", "1000", "2000", "5000"],
+                message: "Please select an option",
+                choices: [
+                    "Withdraw",
+                    "FastCash",
+                    "Balance Inquiry",
+                    "Other Transaction",
+                ],
             },
         ]);
-        myBalance = myBalance - fastCash.fastCash;
-        console.log(`Your remaining balance is ${myBalance}`);
-    }
-    else if (options.menu === "Balance Inquiry") {
-        console.log(`Your available balance is: ${myBalance}`);
-    }
-    else if (options.menu === "Other Transaction") {
-        let trans = await inquirer.prompt([
-            {
-                name: "otherTrans",
-                type: "list",
-                message: "Would You like to other Transaction",
-                choices: ["Yes", "No"],
-            },
-        ]);
-        if (trans.otherTrans === "Yes") {
-            ATM();
+        if (options.menu === "Withdraw") {
+            let amount = await inquirer.prompt([
+                {
+                    name: "amount",
+                    type: "number",
+                    message: "Please enter amount",
+                },
+            ]);
+            if (amount.amount < myBalance) {
+                myBalance -= amount.amount;
+                console.log(`Your Available Balance is ${myBalance}`);
+            }
+            else {
+                console.log(`Insufficient Balance`);
+            }
+        }
+        else if (options.menu === "FastCash") {
+            let fastCash = await inquirer.prompt([
+                {
+                    name: "fastCash",
+                    type: "list",
+                    message: "Select Cash",
+                    choices: ["500", "1000", "2000", "5000"],
+                },
+            ]);
+            myBalance -= parseInt(fastCash.fastCash);
+            console.log(`Your remaining balance is ${myBalance}`);
+        }
+        else if (options.menu === "Balance Inquiry") {
+            console.log(`Your available balance is ${myBalance}`);
+        }
+        else if (options.menu === "Other Transaction") {
+            let trans = await inquirer.prompt([
+                {
+                    name: "otherTrans",
+                    type: "list",
+                    message: "Would You like to other Transaction",
+                    choices: ["Yes", "No"],
+                },
+            ]);
+            if (trans.otherTrans === "Yes") {
+                ATM();
+            }
         }
     }
     else {
-        console.log("Your pin is invalid!");
+        console.log("Incorrect pin entered. Please try again.");
     }
 }
-;
 ATM();
